77de7dcd371ee6b3c3037a24bd25e7bb29379f60
Kaya Erol <kue202@nyu.edu>
2023-11-29 17:45:35 -0500

    Chess Board

--- sketch.js
+++ sketch.js
@@ -1,7 +1,30 @@
 function setup() {
-  createCanvas(400, 400);
-}
+  createCanvas(820, 820); // Canvas Size
+  background(48, 46, 43); // Dark Background
 
-function draw() {
-  background(220);
+  let numSquares = 8;
+  let squareSize = 400 / numSquares; 
+
+  let offsetX = (width - 400) / 2;
+  let offsetY = (height - 400) / 2;
+
+  textSize(16); // Set a larger text size
+  fill(255); // Set a contrasting text color, e.g., red
+
+  for (let i = 0; i < numSquares; i++) {
+    let numberPosStr = str(i + 1); // Convert number to string
+    text(numberPosStr, i * squareSize + offsetX, offsetY - 10); // Adjust Y position for visibility
+
+    for (let j = 0; j < numSquares; j++) {
+      if ((i + j) % 2 == 0) {
+        fill(255);
+      } else {
+        fill(126, 151, 92);
+      }
+      rect(i * squareSize + offsetX, j * squareSize + offsetY, squareSize, squareSize);
+    }
+  }
+
+  noFill();
+  rect(offsetX, offsetY, 400, 400); // Draw the outline around the chessboard
 }
